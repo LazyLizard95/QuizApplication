fb1a6f1b30166af39ffd5df2650c111927790a20
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-19 18:27:23 -0400

    created Repo

---
+++ questionKey.js
@@ -0,0 +1,18 @@
+const firstQuestion = {
+    prompt: "Which of the following is NOT a loop?",
+    answerOne: "For",
+    answerTwo: "While",
+    answerThree: "Do-While",
+    answerFour: "For-While"
+}
+
+const secondQuestion = {
+    prompt: "The function Date.now() will do which of following: ",
+    answerOne: "Return the current date and time",
+    answerTwo: "Return the current date but not time",
+    answerThree: "Return the current time in millieseconds elapsed since January 1, 1970",
+    answerFour: "Return the current time in seconds elapsed since January 1, 1970"
+}
+
+
+let correctAnswers = [firstQuestion.answerFour, secondQuestion.answerThree]
---
+++ script.js
@@ -0,0 +1,29 @@
+let score = 0;
+let timer = 30;
+let startedGame = false;
+let gameTimer = setInterval(startGame, 1000);
+possibleQuestions = [firstQuestion, secondQuestion];
+
+
+$(".start-btn").on('click', function(){
+    $(".start-btn").addClass('hide');
+    startedGame = true;
+    startGame();
+})
+
+function nextQuestion(){
+
+}
+
+function startGame(){
+    if(startedGame){
+    timer--
+    console.log(timer);
+    $(".timerText").text(timer);
+    }
+    if(timer === 0){
+        clearInterval(gameTimer);
+        console.log("gameover")
+    }
+}
+
