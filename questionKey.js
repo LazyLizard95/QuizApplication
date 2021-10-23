3aa41dc252309273bb4d03f97686239bba63b77f
LazyLizard95 <51461957+LazyLizard95@users.noreply.github.com>
2021-10-23 11:58:52 -0400

    Added some questions

--- questionKey.js
+++ questionKey.js
@@ -1,4 +1,3 @@
-
 const firstQuestion = {
     prompt: "Which of the following is NOT a loop?",
     answers = ["For", "While", "Do-While", "For-While"],
@@ -12,52 +11,52 @@ const secondQuestion = {
 }
 
 const thirdQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
+    prompt: "What HTML element is used to enable JavaScript?",
+    answers =["<innerHTML>", "<href = 'script.js'>", "<script>", "<script.js>"],
+    correctAnswer = answers[2]
 }
 
 const fourthQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
+    prompt: "What will the following code display? ('b' + 'a' + + 'a' + 'a')",
+    answers =["baNaNa", "baaa", 'Undefined', 'ba ++ aa'],
+    correctAnswer = answers[0]
 }
 
 const fifthQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
+    prompt: "What is a recursive loop?",
+    answers =["A loop with a sailor mouth", "A loop that repeatedly makes calls to iself", "A loop that doesn't function properly", "Rescursion loops do not exist"],
+    correctAnswer = answers[1]
 }
 
-const sixthQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
-}
+// const sixthQuestion = {
+//     prompt: "",
+//     answers =[],
+//     correctAnswer = answers[]
+// }
 
-const seventhQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
-}
+// const seventhQuestion = {
+//     prompt: "",
+//     answers =[],
+//     correctAnswer = answers[]
+// }
 
-const eigthQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
-}
+// const eigthQuestion = {
+//     prompt: "",
+//     answers =[],
+//     correctAnswer = answers[]
+// }
 
-const ninthQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
-}
+// const ninthQuestion = {
+//     prompt: "",
+//     answers =[],
+//     correctAnswer = answers[]
+// }
 
-const tenthQuestion = {
-    prompt: "",
-    answers =[],
-    correctAnswer = answers[]
-}
+// const tenthQuestion = {
+//     prompt: "",
+//     answers =[],
+//     correctAnswer = answers[]
+// }
 
 
 //need array of questions
